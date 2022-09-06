2b939990b4b4b42ca276ab8a8287b9184e093bcf
jumm2018 <jumm2009@yahoo.de>
2022-09-06 10:20:16 +0200

    End of section 2

---
+++ API/Data/Migrations/20220905145927_chnageQuantityInStock.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    public partial class chnageQuantityInStock : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "QuantityInStockU",
+                table: "Products",
+                newName: "QuantityInStock");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "QuantityInStock",
+                table: "Products",
+                newName: "QuantityInStockU");
+        }
+    }
+}
